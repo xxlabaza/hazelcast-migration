1c90670c01e342a92f984fcb0566c7a7e55aa29e
Artem Labazin <xxlabaza@gmail.com>
2017-01-30 15:53:45 +0300

    Added XML configuration for HZ member

--- member/src/main/java/com/xxlabaza/test/hz/member/Main.java
+++ member/src/main/java/com/xxlabaza/test/hz/member/Main.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright 2017 Artem Labazin - All Rights Reserved.
  *
  * Unauthorized copying of this file, via any medium is strictly prohibited.
@@ -6,12 +6,7 @@
  */
 package com.xxlabaza.test.hz.member;
 
-import static java.util.Collections.singletonList;
-
-import com.hazelcast.config.Config;
-import com.hazelcast.config.NetworkConfig;
 import com.hazelcast.core.Hazelcast;
-import com.xxlabaza.test.hz.model.MyExecutorService;
 
 /**
  * @author Artem Labazin <xxlabaza@gmail.com>
@@ -20,18 +15,18 @@ import com.xxlabaza.test.hz.model.MyExecutorService;
 public class Main {
 
   public static void main(String[] args) {
-    Config config = new Config();
+//    Config config = new Config();
+//
+//    NetworkConfig networkConfig = config.getNetworkConfig();
+//    networkConfig.getJoin().getMulticastConfig().setEnabled(false);
+//    networkConfig.getJoin().getTcpIpConfig().setEnabled(true);
+//    networkConfig.getJoin().getTcpIpConfig().setMembers(singletonList("127.0.0.1"));
+//
+//    config.getExecutorConfig(MyExecutorService.EXECUTOR_NAME)
+//        .setPoolSize(1)
+//        .setQueueCapacity(1)
+//        .setStatisticsEnabled(true);
 
-    NetworkConfig networkConfig = config.getNetworkConfig();
-    networkConfig.getJoin().getMulticastConfig().setEnabled(false);
-    networkConfig.getJoin().getTcpIpConfig().setEnabled(true);
-    networkConfig.getJoin().getTcpIpConfig().setMembers(singletonList("127.0.0.1"));
-
-    config.getExecutorConfig(MyExecutorService.EXECUTOR_NAME)
-        .setPoolSize(1)
-        .setQueueCapacity(1)
-        .setStatisticsEnabled(true);
-
-    Hazelcast.newHazelcastInstance(config);
+    Hazelcast.newHazelcastInstance();
   }
 }
--- model/src/main/java/com/xxlabaza/test/hz/model/MyExecutorService.java
+++ model/src/main/java/com/xxlabaza/test/hz/model/MyExecutorService.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright 2017 Artem Labazin - All Rights Reserved.
  *
  * Unauthorized copying of this file, via any medium is strictly prohibited.
